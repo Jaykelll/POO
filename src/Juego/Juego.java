6d38b03428e1003fc89fa178a6218804d561a6cc
Jaykelll <126786884+Jaykelll@users.noreply.github.com>
2023-03-28 00:22:14 -0500

    Add files via upload

---
+++ src/Juego/Juego.java
@@ -0,0 +1,10 @@
+//crear paquete
+package Juego;
+//crear interfas para jugar dos juegos
+public interface Juego {
+//métodos de los juegos(son los que tienen en comun)
+void jugar();
+void iniciar();
+void finalizar();
+//la interfas solo va a permitir metodos publicos (public) o abstractos (abstrac)
+}
